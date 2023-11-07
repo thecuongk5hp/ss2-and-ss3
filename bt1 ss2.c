d6838b6578d164acc9b833972d99a383929fcd57
thecuongk5hp <147390984+thecuongk5hp@users.noreply.github.com>
2023-11-07 16:42:31 +0700

    Add files via upload

---
+++ bt1 ss2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    float canh, chieu_dai, chieu_rong, ban_kinh, pi = 3.14;
+    
+    float chu vi hinh vuong  = 4 * side;
+    float dien tich hinh vuong = canh * canh;
+    float chu vi hinh chu nhat = 2 * (chieu dai + chieu rong);
+    float dien tich hinh chu nhat = length * ban kinh;
+    float chu vi hinh tron = 2 * pi * ban kinh;
+    float dien tich hinh tron = pi * ban kinh * ban kinh;
+    
+    return 0;
+}
---
+++ th2 ss2.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	int number1 = 11;
+	int number2 = 4;
+	
+	int sum = number1 + number2;
+	int sub = number1 - number2;
+	int mul = number1 * number2;
+	
+	int didvide = number1/number2;
+	int divide_ceil = ceil((double)number1/(double)number2); 
+    int divide_floor = floor((double)number1/(double)number2);  
+    
+    int surplus = number1 % number2; 
+    
+    number1++;
+    number2--;
+    
+    int result = pow(number1,number2);
+} 
